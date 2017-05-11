2d99ff5703fa0ea3781d002edd24b7b6140a92d4
grantweii <z5078316@ad.unsw.edu.au>
2017-05-11 21:33:46 +1000

    box and endzone

--- MVP/src/Box.java
+++ MVP/src/Box.java
@@ -1,11 +1,33 @@
 
 public class Box extends GameObject{
-	@Override
-	public boolean interact(int direction){
-		return false;
+	
+	public Box(){
+		
 	}
+	
+	@Override
+	public boolean interact(int direction){ //simply check if anything is in next grid spot
+		int dx = 0;
+		int dy = 0;
+		if (direction == UP) { 
+			dy = 1;
+		} else if (direction == DOWN) {
+			dy = -1;
+		} else if (direction == LEFT) {
+			dx = -1;
+		} else if (direction == RIGHT) {
+			dx = 1;
+		}
+		GameObject obj = GameManager.getGameManager().getObjectAtLocation(gridX + dx, gridY + dy);
+		if (obj == null || obj instanceof EndZone) {
+			gridX += dx;
+			gridY += dy;
+			return true;
+		} else return false;
+	}	
+		
 	@Override
 	public void act(){
-		
+		super.act();
 	}
 }
--- MVP/src/EndZone.java
+++ MVP/src/EndZone.java
@@ -1,14 +1,23 @@
 
 public class EndZone extends GameObject{
+	private boolean activated;
+	
+	public EndZone() {
+		activated = false;
+	}
+	
 	@Override
 	public void act(){
-		
+		if (GameManager.getGameManager().getObjectAtLocation(gridX, gridY) instanceof Box)
+		activated = true;
 	}
+	
 	@Override
 	public boolean interact(int direction){
 		return true;
 	}
+	
 	public boolean getActive(){
-		return false;
+		return activated;
 	}
 }
