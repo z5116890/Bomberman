47862cf142998aa066ec2eea50e98499fd83ad84
Austin <z5116890@ad.unsw.edu.au>
2017-05-11 21:46:03 +1000

    player interact now updates position when true

--- MVP/src/Player.java
+++ MVP/src/Player.java
@@ -1,14 +1,7 @@
 
 public class Player extends GameObject{
 	
-	private int posX;
-	private int posY;
-	
-	//initial position of player depends on gameManager setup for map/player generation
-	public Player(int x, int y){
-		this.setPosX(x);
-		this.setPosY(y);
-	}
+
 	
 	//things that interact with player
 	//nothing interacts with player?
@@ -17,13 +10,25 @@ public class Player extends GameObject{
 	public boolean interact(int direction){
 		//see if anything is in the way ie box, wall
 		if(direction == 1){
-			if(this.movable(this.posX, this.posY + 1)) return true;
+			if(this.movable(this.gridX, this.gridY + 1)){
+				this.gridY++;
+				return true;
+			}
 		}else if(direction == 2){
-			if(this.movable(this.posX, this.posY - 1)) return true;
+			if(this.movable(this.gridX, this.gridY - 1)){
+				this.gridY--;
+				return true;
+			}
 		}else if(direction == 3){
-			if(this.movable(this.posX - 1, this.posY)) return true;
+			if(this.movable(this.gridX - 1, this.gridY)){
+				this.gridX--;
+				return true;
+			}
 		}else if(direction == 4){
-			if(this.movable(this.posX + 1, this.posY)) return true;
+			if(this.movable(this.gridX + 1, this.gridY)){
+				this.gridX++;
+				return true;
+			}
 		}
 				//object exists in that location so return false
 		return false;
@@ -32,24 +37,9 @@ public class Player extends GameObject{
 	
 	
 	public void act(){
-		
+		super.act();
 	}
 
-	public int getPosX() {
-		return posX;
-	}
-
-	public void setPosX(int posX) {
-		this.posX = posX;
-	}
-
-	public int getPosY() {
-		return posY;
-	}
-
-	public void setPosY(int posY) {
-		this.posY = posY;
-	}
 	
 	public boolean movable(int x, int y){
 		if(GameManager.getGameManager().getObjectAtLocation(x, y) != null) return false;
