f09a3674a626f75b3eba23dccfdcef651457b715
Daniel Kennedy <dannykennedy@email.com>
2017-05-11 15:13:25 +1000

    added code to wall class

--- MVP/src/Wall.java
+++ MVP/src/Wall.java
@@ -1,4 +1,29 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class Wall extends GameObject{
-	
+
+    public Wall (int x, int y){
+
+        this.gridX = x;
+        this.gridY = y;
+        this.realX = x*GameManager.CELL_SIZE;
+        this.realY = y*GameManager.CELL_SIZE;
+
+        //add image
+        BufferedImage img = null;
+        try{
+            //this image is already sized to 32x32px, i.e. CELL_SIZE*CELL_SIZE
+            img = ImageIO.read(new File("wall_dummy.jpg"));
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        this.image = img;
+
+        //System.out.println("making a wall");
+    }
+
 }
