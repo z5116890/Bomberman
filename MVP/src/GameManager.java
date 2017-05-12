a96d38709ac3fb8bfe31e887bfeba60fb478f3f4
Daniel Kennedy <dannykennedy@email.com>
2017-05-12 12:37:40 +1000

    Added “game speed” setting in options menu. Not currently connected to anything

--- MVP/src/GameManager.java
+++ MVP/src/GameManager.java
@@ -2,11 +2,12 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import javax.imageio.ImageIO;
 import javax.sound.sampled.AudioInputStream;
@@ -19,23 +20,23 @@ public class GameManager{
 	public static final int CELL_COUNT = 20;
 	public static final int CELL_SIZE = MAP_SIZE/CELL_COUNT;//32
 	public static final int TARGET_FRAME_RATE = 32;
-	
+
 	//map generation stuff
 	private static final int EMPTY = 0;
 	private static final int WALL = 1;
 	private static final int BOX = 2;
 	private static final int PLAYER = 3;
 	private static final int ENDZONE = 4;
-	
-	
+
+
 	private static GameManager gm;
-		
+
 	//Game State
 	private boolean gameStarted = false;
 	private ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
 	private ArrayList<EndZone> endZones = new ArrayList<EndZone>();
 	private Player player;
-	
+
 	//Swing stuff
 	private JFrame frame;
 	private PaintingPanel panel;
@@ -43,18 +44,18 @@ public class GameManager{
 	private JTextField txtName;
 	private final ButtonGroup radios = new ButtonGroup();
 	private JLabel lblRadio = new JLabel("radio1");
-	
+
 	private GameManager(){
 		gm = this;
 		panel = new PaintingPanel();
-		
+
 		frame = new JFrame("Bomberman");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//frame.add(panel);
 		//frame.pack();
 		frame.setSize(panel.getPreferredSize());
 		frame.setVisible(true);
-		
+
 		createMap();
 	}
 	//Creates an instance of GameManager, does start screen, runs the game
@@ -91,13 +92,13 @@ public class GameManager{
 				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,1,1},
 				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
 		};
-		
+
 		//Create map from array
 		ArrayList<GameObject> walls = new ArrayList<GameObject>();
 		ArrayList<EndZone> endzones = new ArrayList<EndZone>();
 		ArrayList<GameObject> boxes = new ArrayList<GameObject>();
 		ArrayList<Player> players = new ArrayList<Player>();
-		
+
 		for(int y = 0;y<map.length;y++){
 			for(int x = 0;x<map[y].length;x++){
 				switch(map[y][x]){
@@ -118,17 +119,17 @@ public class GameManager{
 				}
 			}
 		}
-		
+
 		gameObjects.addAll(walls);
 		gameObjects.addAll(boxes);
 		endZones.addAll(endzones);
-		player = players.get(0);//there should be one and only one of these for now. 
-		
+		player = players.get(0);//there should be one and only one of these for now.
+
 		panel.addGameObjects(walls);
 		panel.addGameObjects(endZones);
 		panel.addGameObjects(boxes);
 		panel.addGameObject(player);
-		
+
 	}
 	//all of the actual game is run in here, the method doesn't end until the game is over
 	public void runGame(){
@@ -144,14 +145,14 @@ public class GameManager{
 		boolean ended = false;
 		while(!ended){
 			time = System.nanoTime();
-			
+
 			if(player!=null)//temporary if statement
-			player.act();
-			
+				player.act();
+
 			for(GameObject obj:gameObjects){
 				obj.act();
 			}
-			
+
 			ended = true;
 			for(EndZone e:endZones){
 				e.act();
@@ -161,7 +162,7 @@ public class GameManager{
 			//limits the frame rate
 			while(System.nanoTime()-time<(long)(1000000000L/TARGET_FRAME_RATE));
 		}
-		
+
 	}
 	//returns the object at the specified grid location, if multiple objects are in the same place, it returns the one rendered last (seen ontop)
 	public GameObject getObjectAtLocation(int x,int y){
@@ -175,6 +176,7 @@ public class GameManager{
 	public static GameManager getGameManager(){
 		return gm;
 	}
+
 	private void startMenu(){
 		//playing music
 		Clip clip = null;
@@ -199,16 +201,94 @@ public class GameManager{
 		JLabel background = new JLabel(imageIcon);
 		background.setVisible(true);
 		background.setSize(PaintingPanel.DEFAULT_WINDOW_SIZE,PaintingPanel.DEFAULT_WINDOW_SIZE);
-		
+
 		JPanel tempPanel = new JPanel();
 		frame.add(tempPanel);
 		tempPanel.add(background);
 		background.setVisible(true);
-		
+
+		addStartMenuButtons(background);
+
+
+
+
+		frame.pack();
+		frame.setSize(640,640);
+		while(!gameStarted)System.out.print("");
+		frame.remove(tempPanel);
+		clip.stop();
+		clip.close();
+
+	} //start menu
+
+	/**
+	 * Create the options menu buttons, add main settings buttons back on return
+	 * Precondition: the settings buttons have already been removed
+	 * @param background - the main image in the menu
+	 */
+	private void addOptionsMenuButtons(JLabel background){
+
+		//At this point we're already in the start menu, there's no other way to get here
+
+		//Slider to control game speed - parameters are max, min and default values
+		JSlider speedSlider = new JSlider(JSlider.HORIZONTAL,
+				1, 5, 3);
+
+		speedSlider.setSize(new Dimension(100,500));
+		speedSlider.setBounds(310,295,100,50);
+		Font font = new Font("Impact", Font.PLAIN, 15);
+		speedSlider.setFont(font);
+		speedSlider.setForeground(Color.white);
+		speedSlider.setMajorTickSpacing(1);
+		speedSlider.setPaintLabels(true);
+		speedSlider.setSnapToTicks(true);
+
+		speedSlider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent event) {
+				int value = speedSlider.getValue();
+
+				System.out.println("Setting speed to " + value);
+			}
+		});
+
+		JLabel speedLabel = new JLabel("Game speed");
+		speedLabel.setFont(font);
+		speedLabel.setForeground(Color.white);
+		//Might be nice if this wasn't so hard coded...
+		speedLabel.setBounds(220,308,200,50);
+
+		//Add label and slider to background
+		background.add(speedSlider);
+		background.add(speedLabel);
+
+		//make a back button so that we can go back to the normal options menu
+		//creating start button
+		JButton btnBack = new JButton("Back to main menu");
+		btnBack.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				background.remove(btnBack);
+				background.remove(speedSlider);
+				background.remove(speedLabel);
+				addStartMenuButtons(background);
+				background.repaint();
+			}
+		});
+		btnBack.setFont(new Font("Impact", Font.PLAIN,16));
+		btnBack.setForeground(new Color(208,17,8));
+		btnBack.setSize(new Dimension(100,500));
+		btnBack.setBounds(220,225,200,50);
+
+		background.add(btnBack);
+
+	}
+
+	private void addStartMenuButtons(JLabel background){
+
 		//creating start button
 		JButton btnStart = new JButton("Play Bomberman");
 		btnStart.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
+
 				GameManager.getGameManager().startGame();
 				btnStart.setForeground(new Color(0,0,255));
 			}
@@ -225,6 +305,7 @@ public class GameManager{
 		btnHighScores.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 
+
 				btnHighScores.setForeground(new Color(0,0,255));
 			}
 		});
@@ -240,7 +321,11 @@ public class GameManager{
 		btnOptions.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 
-				btnOptions.setForeground(new Color(0,0,255));
+				background.remove(btnHighScores);
+				background.remove(btnStart);
+				background.remove(btnOptions);
+				addOptionsMenuButtons(background);
+				background.repaint();
 			}
 		});
 		btnOptions.setFont(new Font("Impact", Font.PLAIN,16));
@@ -251,31 +336,28 @@ public class GameManager{
 		background.add(btnOptions);
 
 
-		frame.pack();
-		frame.setSize(640,640);
-		while(!gameStarted)System.out.println("");
-		frame.remove(tempPanel);
-		clip.stop();
-		clip.close();
-		
 	}
+
+
+
+
 	public void startGame(){
 		gameStarted = true;
 	}
 	public void keyPressed(int keyCode){
 		switch(keyCode){
-		case KeyEvent.VK_UP:
-			player.setAction(GameObject.UP);
-			break;
-		case KeyEvent.VK_DOWN:
-			player.setAction(GameObject.DOWN);
-			break;
-		case KeyEvent.VK_LEFT:
-			player.setAction(GameObject.LEFT);
-			break;
-		case KeyEvent.VK_RIGHT:
-			player.setAction(GameObject.RIGHT);			
-			break;
+			case KeyEvent.VK_UP:
+				player.setAction(GameObject.UP);
+				break;
+			case KeyEvent.VK_DOWN:
+				player.setAction(GameObject.DOWN);
+				break;
+			case KeyEvent.VK_LEFT:
+				player.setAction(GameObject.LEFT);
+				break;
+			case KeyEvent.VK_RIGHT:
+				player.setAction(GameObject.RIGHT);
+				break;
 		}
 	}
 }
