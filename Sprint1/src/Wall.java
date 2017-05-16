ffcfae11f947c1828b055fa8b27ef18bfc45c532
Austin <z5116890@ad.unsw.edu.au>
2017-05-16 13:24:05 +1000

    included boolean destructible and now waiting for bomb explosion stuff to be made

--- Sprint1/src/Wall.java
+++ Sprint1/src/Wall.java
@@ -4,28 +4,33 @@ import java.io.File;
 import java.io.IOException;
 
 public class Wall extends GameObject{
-
+	private boolean destructable;
+	
+	
     public Wall (int x, int y){
+    	
     	super("Wall.png",x,y);
-    	/*
-        this.gridX = x;
-        this.gridY = y;
-        this.realX = x*GameManager.CELL_SIZE;
-        this.realY = y*GameManager.CELL_SIZE;
-
-        //add image
-        BufferedImage img = null;
-        try{
-            //this image is already sized to 32x32px, i.e. CELL_SIZE*CELL_SIZE
-            img = ImageIO.read(new File("wall_dummy.jpg"));
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-
-        this.image = img;
-
-        //System.out.println("making a wall");
-       */
+    	
     }
+
+
+	public boolean isDestructable() {
+		return destructable;
+	}
+
+
+	public void setDestructable(boolean destructable) {
+		this.destructable = destructable;
+	}
+	
+	public void destroy(){
+		//if bomb explosion radius includes wall position then destroy wall
+		if(this.destructable == true){
+			//if(explosion includes wall position then destroy wall){
+			
+			//}
+		}
+		
+	}
 
 }
